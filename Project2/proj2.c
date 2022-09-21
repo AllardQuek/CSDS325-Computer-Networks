347ad856099eec1301aa2a01b31dc56d9c963531
Allard Quek <allard.quek@comp.nus.edu.sg>
2022-09-21 03:05:53 +0000

    Start parsing arguments

--- Project2/proj2.c
+++ Project2/proj2.c
@@ -1,12 +1,81 @@
-#include <ctype.h>
+/**
+ * CSDS 325 Project 2
+ * 
+ * The second project of the semester involves writing a simple command line-based web client. 
+ * The aim of this project is 
+ * (i) to get your feet wet with writing C/C++, 
+ * (ii) to write a program that exchanges information with another computer over a network and 
+ * (iii) to start concretely thinking about protocols.
+ * */
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define ARG_HELP  0x1
+#define ARG_MSG   0x2
+#define ARG_HELLO 0x4
+
+unsigned short cmd_line_flags = 0;
+char *msg = NULL;
+
+
+void usage (char *progname)
+{
+    fprintf (stderr,"%s [-h] [-s] [-m message]\n", progname);
+    fprintf (stderr,"   -h    show program usage\n");
+    fprintf (stderr,"   -s    print standard greeting\n");
+    fprintf (stderr,"   -m X  print message \'X\'\n");
+    exit (1);
+}
+
+
+void parseargs (int argc, char *argv [])
+{
+    int opt;
+
+    while ((opt = getopt (argc, argv, "hsm:")) != -1)
+    {
+        switch (opt)
+        {
+            case 'h':
+              cmd_line_flags |= ARG_HELP;
+              break;
+            case 's':
+              cmd_line_flags |= ARG_HELLO;
+              break;
+            case 'm':
+              cmd_line_flags |= ARG_MSG;
+              msg = optarg;
+              break;
+            case '?':
+            default:
+              usage (argv [0]);
+        }
+    }
+    if (cmd_line_flags == 0)
+    {
+        fprintf (stderr,"error: no command line option given\n");
+        usage (argv [0]);
+    }
+}
+
 
 int main (int argc, char *argv [])
 {
-	char str[] = "HELLO world";
+    parseargs (argc,argv);
 
-	for(int i = 0; str[i]; i++){
-		str[i] = tolower(str[i]);
-	}
-	printf("%s\n", str);
+    if (cmd_line_flags == ARG_HELP)
+        usage (argv [0]);
+    else if (cmd_line_flags == ARG_HELLO)
+        fprintf (stdout,"hello world\n");
+    else if (cmd_line_flags == ARG_MSG)
+        fprintf (stdout,"%s\n", msg);
+    else
+    {
+        fprintf (stderr,"error: only one option at a time allowed\n");
+        exit (1);
+    }
+    exit (0);
 }
