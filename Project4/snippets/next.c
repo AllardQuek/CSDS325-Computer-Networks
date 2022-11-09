8b0fb128fae83d7a0fff639daa2b4d5b355af77b
Allard <AllardQuek@users.noreply.github.com>
2022-11-09 11:32:58 +0800

    Walk through snippet code

--- Project4/snippets/next.c
+++ Project4/snippets/next.c
@@ -31,44 +31,59 @@ unsigned short next_packet (int fd, struct pkt_info *pinfo)
     struct meta_info meta;
     int bytes_read;
 
+    // Clear out memory in both structs
     memset (pinfo,0x0,sizeof (struct pkt_info));
     memset (&meta,0x0,sizeof (struct meta_info));
 
-    /* read the meta information */
+    /* 1. read the meta information (12 bytes) */
     bytes_read = read (fd,&meta,sizeof (meta));
     if (bytes_read == 0)
         return (0);
     if (bytes_read < sizeof (meta))
         errexit ("cannot read meta information");
-    pinfo->caplen = ntohs (meta.caplen);
-    /* set pinfo->now based on meta.secs & meta.usecs */
+
+    // Set caplen attribute of pkt_info struct
+    pinfo->caplen = ntohs (meta.caplen);    
+
+    /* 2. set pinfo->now based on meta.secs & meta.usecs */
     if (pinfo->caplen == 0)
         return (1);
     if (pinfo->caplen > MAX_PKT_SIZE)
         errexit ("packet too big");
-    /* read the packet contents */
-    bytes_read = read (fd,pinfo->pkt,pinfo->caplen);
+
+    /* 3. read the packet contents (caplen bytes) */
+    bytes_read = read (fd, pinfo->pkt,pinfo->caplen);
+
+    // Some error checking
     if (bytes_read < 0)
         errexit ("error reading packet");
     if (bytes_read < pinfo->caplen)
         errexit ("unexpected end of file encountered");
     if (bytes_read < sizeof (struct ether_header))
         return (1);
+
+    // Set ethernet header (first 14 bytes)
     pinfo->ethh = (struct ether_header *)pinfo->pkt;
     pinfo->ethh->ether_type = ntohs (pinfo->ethh->ether_type);
+
+    // Ignore anything that is not IP
     if (pinfo->ethh->ether_type != ETHERTYPE_IP)
         /* nothing more to do with non-IP packets */
         return (1);
     if (pinfo->caplen == sizeof (struct ether_header))
         /* we don't have anything beyond the ethernet header to process */
         return (1);
+
     /* set pinfo->iph to start of IP header */
+
     /* if TCP packet, 
           set pinfo->tcph to the start of the TCP header
           setup values in pinfo->tcph, as needed */
+
     /* if UDP packet, 
           set pinfo->udph to the start of the UDP header,
           setup values in pinfo->udph, as needed */
+
+    // * TODO -s: turn two timestamps into double
     return (1);
 }
-
